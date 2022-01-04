5057f43f9bbe40bc00c676be824d70e06de17d02
gurrappa <gurappavajjarala@gmail.com>
2022-01-04 16:12:02 +0530

    my project

---
+++ vajrala/src/main/java/com/example/vajrala/VajralaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vajrala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VajralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VajralaApplication.class, args);
+	}
+
+}
---
+++ vajrala/src/test/java/com/example/vajrala/VajralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vajrala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VajralaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
