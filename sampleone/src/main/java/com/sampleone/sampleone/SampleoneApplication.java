966dd767333a4f13525a0d0a17b03355015365c9
gurrappa <gurappavajjarala@gmail.com>
2022-01-05 16:15:33 +0530

    testing sample one

---
+++ sampleone/src/main/java/com/sampleone/sampleone/SampleoneApplication.java
@@ -0,0 +1,13 @@
+package com.sampleone.sampleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleoneApplication.class, args);
+	}
+
+}
---
+++ sampleone/src/test/java/com/sampleone/sampleone/SampleoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleone.sampleone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
